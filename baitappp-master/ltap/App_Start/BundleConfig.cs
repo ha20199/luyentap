c4acf330b621b18a659907926abfdbce5771884a
QUACH MANH HA <1721050340@student.humg.edu.vn>
2021-09-28 22:13:19 +0700

    Done ckeditor

--- baitappp-master/ltap/App_Start/BundleConfig.cs
+++ baitappp-master/ltap/App_Start/BundleConfig.cs
@@ -10,6 +10,8 @@ namespace ltap
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ckEditor").Include(
+                        "~/Scripts/ckeditor/ckeditor.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- baitappp-master/ltap/Models/Person.cs
+++ baitappp-master/ltap/Models/Person.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ltap.Models
 {
@@ -10,6 +11,7 @@ namespace ltap.Models
     {
         [Key]
         public string PersonID { get; set; }
+        [AllowHtml]
         public string PersonName { get; set; }        
     }
 }
